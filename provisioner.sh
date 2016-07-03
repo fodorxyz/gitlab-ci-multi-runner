88f5d51cb49f01df6d8aa2a0637b456840636f3e
Ashley Hindle <hey@ashleyhindle.com>
2016-07-03 12:42:56 +0100

    Initial setup fodor.json/provisioner.sh

---
+++ provisioner.sh
@@ -0,0 +1,10 @@
+export REGISTER_NON_INTERACTIVE=true
+
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | bash
+apt-get -y update
+apt-get install gitlab-ci-multi-runner
+
+
+# We use fodor's input system to set the needed env variables so this will work headlessly
+# assuming all the info was right
+gitlab-runner register
