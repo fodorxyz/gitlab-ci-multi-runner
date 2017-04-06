e3d7a56ad0fda099af1e389d0183593b435bce09
Ashley Hindle <ashleyhindle@users.noreply.github.com>
2017-04-06 12:41:45 +0100

    Update provisioner to use the docker executor

--- provisioner.sh
+++ provisioner.sh
@@ -1,10 +1,16 @@
 export REGISTER_NON_INTERACTIVE=true
+export DEBIAN_FRONTEND=noninteractive
 
-curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | bash
+# Install docker
+curl -sSL https://get.docker.com/ | sh
+
+
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
+
 apt-get -y update
 apt-get install gitlab-ci-multi-runner
 
 
 # We use fodor's input system to set the needed env variables so this will work headlessly
 # assuming all the info was right
-gitlab-runner register --executor shell
+gitlab-runner register --executor docker
