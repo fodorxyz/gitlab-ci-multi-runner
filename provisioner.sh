6b866ada267adf116f0983d3a7f20968b38bfb87
Ashley Hindle <ashleyhindle@users.noreply.github.com>
2017-04-06 14:06:38 +0100

    Remove unnecessary apt-get update line
    The GitLab script.deb.sh does that for us

--- provisioner.sh
+++ provisioner.sh
@@ -3,7 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
 
-apt-get -y update
 apt-get install gitlab-ci-multi-runner
 
 
